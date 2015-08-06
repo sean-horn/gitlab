e04df22c14bced42408133b8de744871e65fa478
Sean Horn <sean_horn@opscode.com>
2015-08-05 21:36:28 -0400

    A change to cleanup recipes 6

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,7 +12,7 @@ require 'uri'
 include_recipe 'sshd'
 include_recipe 'postfix'
 include_recipe 'gitlab::_openssl'\
-if URI(node['gitlab']['external_url']).scheme == 'https'
+ if URI(node['gitlab']['external_url']).scheme == 'https'
 
 execute 'gitlab-ctl reconfigure' do
   action :nothing
