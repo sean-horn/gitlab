b7bfe3fdc0da4a14ee9ae71c96c3ac9973c15e62
Sean Horn <sean_horn@opscode.com>
2015-08-05 21:31:27 -0400

    A change to cleanup recipes 3

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,38 +11,21 @@ require 'uri'
 
 include_recipe 'sshd'
 include_recipe 'postfix'
-
 include_recipe 'gitlab::_openssl' if URI(node['gitlab']['external_url']).scheme == 'https'
 
-execute 'gitlab-ctl reconfigure' do
+execute "gitlab-ctl reconfigure" do
   action :nothing
 end
 
 unless node['gitlab']['omnibus']['in_repo']
-  distro = '#{node['platform']}-#{node['platform_version'].split('.').first}'
+  distro = "#{node['platform']}-#{node['platform_version'].split('.').first}"
   download_url = node['gitlab']['omnibus']['url']['distro']
   filename = File.basename(node['gitlab']['omnibus']['url']['distro'])
   chksm = node['gitlab']['omnibus']['checksum']['distro']
   
-  remote_file '#{Chef::Config['file_cache_path']}/#{filename}' do
+  remote_file "#{Chef::Config['file_cache_path']}/#{filename}" do
     source download_url
     mode '0644'
     checksum chksm
   end
-end
-
-package 'gitlab' do
-  source '#{Chef::Config['file_cache_path']}/#{filename}' unless node['gitlab']['omnibus']['in_repo']
-  action :install
-end
-
-template '/etc/gitlab/gitlab.rb' do
-  source 'gitlab.rb.erb'
-  owner 'root'
-  group 'root'
-  mode '0600'
-  variables(
-    :external_url => node['gitlab']['external_url']
-  )
-  notifies :run, 'execute[gitlab-ctl reconfigure]'
 end
