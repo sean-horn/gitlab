b73334c33aa3fa4b03d93ad3d4778f52ae284406
Sean Horn <sean_horn@opscode.com>
2015-08-05 21:09:22 -0400

    A change to cleanup

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
-name             'gitlab'
-maintainer       'Stephen Lauck'
+name 'gitlab'
+maintainer 'Stephen Lauck'
 maintainer_email 'lauck@getchef.com'
-license          'All rights reserved'
-description      'Installs/Configures gitlab'
+license 'All rights reserved'
+description 'Installs/Configures gitlab'
 long_description 'Installs/Configures gitlab'
-version          '0.2.3'
+version '0.2.3'
 
-%w{ sshd postfix yum apt openssl }.each do |cb|
+%w( sshd postfix yum apt openssl ).each do |cb|
   depends cb
 end
